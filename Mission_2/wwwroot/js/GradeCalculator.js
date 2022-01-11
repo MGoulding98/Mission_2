4cb91294fae19cea17cef028293a01186b79c0b1
Gouldking <59857002+Gouldking@users.noreply.github.com>
2022-01-10 20:17:45 -0700

    Add project files.

---
+++ Mission_2/wwwroot/js/GradeCalculator.js
@@ -0,0 +1,16 @@
+﻿// Runs when the calculate button is pressed
+$("#calculate").click(function () {
+    var finalGrade = 0;
+
+    // Calculates the final grade by adding up each entry from the form and multiplying them by their respective weights. 
+    finalGrade =
+
+        (parseInt($("#assignments").val()) * .55 +
+            parseInt($("#groupProjects").val()) * .05 +
+            parseInt($("#quizzes").val()) * .10 +
+            parseInt($("#exams").val()) * .20 +
+            parseInt($("#intex").val()) * .10);
+
+    // Displays grade to the user after calculation is finished 
+    alert("Final Grade = " + String(finalGrade) + "%");
+})
